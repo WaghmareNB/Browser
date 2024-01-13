aea0f1b6746888c26f83e407060a8723efe2c062
NBW <waghamarenandkishor2093@gmail.com>
2024-01-13 21:20:02 +0530

    just updated

--- browser launch.py
+++ browser launch.py
@@ -4,6 +4,7 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select
+from selenium.webdriver import ActionChains
 
 chrome_options = Options()
 chrome_options.add_experimental_option(name="detach", value=True)
@@ -17,15 +18,37 @@ driver.find_element(By.ID,'username').send_keys("Bright@123")
 driver.find_element(By.ID,'password').send_keys("pass123")
 driver.find_element(By.XPATH,'/html/body/div[1]/div/form/div[3]/div/button').click()
 driver.find_element(By.XPATH,'//*[@id="homeSubmenu"]/li[1]/a').click()
-time.sleep(5)
 driver.find_element(By.XPATH,"//*[@id='fname']").send_keys("Nandkishor")
 driver.find_element(By.XPATH,'//*[@id="lname"]').send_keys("Waghmare")
 a = Select(driver.find_element(By.XPATH,"//*[@id='country']"))
 a.select_by_index("0")
 driver.find_element(By.XPATH,'//*[@id="subject"]').send_keys("Automestion Testing")
-button = driver.find_element(By.XPATH,'//*[@id="content"]/div/form/div[5]/input').click()
-button.click()
-driver.get("https://brightinfo.in/Text%20Box.html")
+# button = driver.find_element(By.XPATH,'//*[@id="content"]/div/form/div[5]/input').click()
+# button.click()
+# driver.get("https://brightinfo.in/Text%20Box.html")
+# driver.back()
+driver.find_element(By.XPATH,'//*[@id="homeSubmenu"]/li[2]/a').click()
+driver.find_element(By.XPATH,'//*[@id="myCheck"]').click()
+driver.find_element(By.XPATH,'//*[@id="homeSubmenu"]/li[3]/a').click()
+driver.find_element(By.XPATH,'//*[@id="yesCheck"]').click()
+driver.find_element(By.XPATH,'//*[@id="homeSubmenu"]/li[4]/a').click()
+driver.find_element(By.XPATH,'//*[@id="homeSubmenu"]/li[5]/a').click()
+act=ActionChains(driver)
+menu=driver.find_element(By.XPATH,"//*[@id='content']/button[1]")
+act.double_click(menu).perform()
+act=ActionChains(driver)
+menu=driver.find_element(By.XPATH,'//*[@id="content"]/button[2]')
+act.click(menu).perform()
+act=ActionChains(driver)
+menu=driver.find_element(By.XPATH,'//*[@id="content"]/button[3]')
+act.context_click(menu).perform()
+driver.find_element(By.XPATH,'//*[@id="homeSubmenu"]/li[6]/a').click()
+driver.find_element(By.ID,'simpleLink').click()
+driver.get("https://brightinfo.in/Links.html")
+time.sleep(3)
 driver.back()
+
+
+
 
 
