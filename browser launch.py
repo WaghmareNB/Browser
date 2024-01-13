3dff9d239c129954ecef5e2dc89552785f886c6e
NBW <waghamarenandkishor2093@gmail.com>
2024-01-13 15:23:39 +0530

    update

--- browser launch.py
+++ browser launch.py
@@ -3,6 +3,7 @@ import time
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import Select
 
 chrome_options = Options()
 chrome_options.add_experimental_option(name="detach", value=True)
@@ -15,20 +16,16 @@ driver.maximize_window()
 driver.find_element(By.ID,'username').send_keys("Bright@123")
 driver.find_element(By.ID,'password').send_keys("pass123")
 driver.find_element(By.XPATH,'/html/body/div[1]/div/form/div[3]/div/button').click()
-driver.find_element (By.XPATH,'//*[@id="homeSubmenu"]/li[2]/a').click()
-chkbox=driver.find_element((By.XPATH,'//*[@id="myCheck"]'))
-if chkbox.is_selected():
-    pass
-else:
-    chkbox.click()
-chkbox1=driver.find_element(By.XPATH,'//*[@id="myCheck"]')
-if chkbox1.is_selected():
-    pass
-else:
-    chkbox.click()
-chkbox2=driver.find_element(By.XPATH,'//*[@id="myCheck"]')
-if chkbox2.is_selected():
-   pass
-else:
-    chkbox2.click()
+driver.find_element(By.XPATH,'//*[@id="homeSubmenu"]/li[1]/a').click()
+time.sleep(5)
+driver.find_element(By.XPATH,"//*[@id='fname']").send_keys("Nandkishor")
+driver.find_element(By.XPATH,'//*[@id="lname"]').send_keys("Waghmare")
+a = Select(driver.find_element(By.XPATH,"//*[@id='country']"))
+a.select_by_index("0")
+driver.find_element(By.XPATH,'//*[@id="subject"]').send_keys("Automestion Testing")
+button = driver.find_element(By.XPATH,'//*[@id="content"]/div/form/div[5]/input').click()
+button.click()
+driver.get("https://brightinfo.in/Text%20Box.html")
+driver.back()
+
 
